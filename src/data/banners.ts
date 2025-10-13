75e05456d38615e1437f25b74dbaf5e85d5c3aa8
Matthew Gerstman <matthew.gerstman@gmail.com>
2025-10-13 19:08:26 +0000

    Add HomeKit banner image to banners.ts
    - Added 'all-in-on-homekit': '/images/blog/01-1.jpeg' to bannerImages
    - This is the correct way to add banner images in this blog
    - Removed incorrect feature_image field from posts.json
    - Banner will now display at the top of the HomeKit post

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -17,4 +17,5 @@ export const bannerImages: { [key: string]: string } = {
   'what-the-functor': '/images/blog/Functor-Wood.jpg',
   'everything-i-wish-i-knew-before-my-first-bay-area-internship': '/images/blog/Bay-Bridge.jpg',
   'mary-had-a-little-lambda': '/images/blog/Lambda-Tree.jpg',
+  'all-in-on-homekit': '/images/blog/01-1.jpeg',
 };
