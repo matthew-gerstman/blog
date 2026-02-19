74aab3865d287f16c4cb775e71f56642abde2321
Matthew Gerstman <mattgerstman@gmail.com>
2026-02-19 21:32:43 +0000

    fix(what-the-functor): inline gists so code renders

--- src/data/posts/what-the-functor.ts
+++ src/data/posts/what-the-functor.ts
@@ -20,9 +20,33 @@ const post: Post = {
 <p>What fun would it be if we didn't start with the titular term, functor.</p>
 <p>A <em>functor </em>is anything that can be mapped over. This is most commonly a list, but really it's any object that can be mapped over.</p>
 <p>For example, we can make a <code>Wizard</code> that can be mapped over.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/4f20482a242e565ea455ebb78f0a617a.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+type Wizard = {
+  name: string;
+  house: string;
+};
+
+const wizard: Wizard = {
+  name: 'Harry',
+  house: 'Gryffindor',
+};
+
+type Mappable<T> = {
+  map<U>(fn: (val: T) => U): Mappable<U>;
+};
+
+const mappableWizard: Mappable<Wizard> = {
+  map: function<U>(fn: (val: Wizard) => U) {
+    return {
+      map: function<V>(fn2: (val: U) => V) {
+        return mappableWizard.map(w => fn2(fn(w)));
+      }
+    };
+  }
+};
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p><em>Note: in most practical situations, a functor would be parametric (containing a type parameter like <code>String</code> in <code>Array&lt;String&gt;</code>), but <code>Wizard</code> does fulfill the basic definition of a functor.</em></p>
 <p>Now the <code>map</code> function also needs to meet the following criteria.</p>
 <h2 id="identity-law-1-">Identity Law &nbsp;<a href="https://medium.com/@dtinth/what-is-a-functor-dcf510b098b6">[1]</a></h2>
@@ -32,9 +56,19 @@ const post: Post = {
 <h2 id="composition-law-1-">Composition Law &nbsp;<a href="https://medium.com/@dtinth/what-is-a-functor-dcf510b098b6">[1]</a></h2>
 <p><code>functor.map(x =&gt; f(g(x))) === functor.map(g).map(f)</code></p>
 <p>Woah that's intimidating. What does this actually mean? Let's look at an example.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/a9a6c8392e87e7a936a3e383fa0f2be5.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+const joinGryffindor = (w: Wizard): Wizard => ({
+  ...w,
+  house: 'Gryffindor'
+});
+
+const learnExpelliarmous = (w: Wizard): Wizard => ({
+  ...w,
+  spells: [...(w.spells || []), 'Expelliarmous']
+});
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>We have <code>joinGryffindor</code> and <code>learnExpelliarmous</code>. If we call </p>
 <p><code>wizard.map(joinGryffindor).map(learnExpelliarmous)</code> it needs to be equivalent to &nbsp;<code>wizard.map(w =&gt; learnExpelliarmous(joinGryffindor(w)))</code>. </p>
 <p>Now it's worth calling out, it needs to be functionally equivalent. In abstract mathematics we're not worried about pointers and references so we can consider this good enough. This is also known as "Fast and Loose Reasoning is morally correct." <a href="http://www.cse.chalmers.se/~nad/publications/danielsson-et-al-popl2006.html">[13]</a></p>
@@ -66,22 +100,56 @@ const post: Post = {
 <li><strong>Associative: </strong>This states that the order of operations remains constant so <code>(a +b) + c === a + (b + c)</code>.</li>
 </ul>
 <p>If these seem vague and generalized, well thats how abstract math works. Let's look at a practical example, a string monoid. <a href="http://s3.amazonaws.com/erlang-conferences-production/media/files/000/000/760/original/Daniela_Sfregola_-_A_Pragmatic_Introduction_to_Category_Theory.pdf?1510238446">[9]</a></p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/3f97a78df599085a01bd8663e80cc9ec.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+type StringMonoid = {
+  identity: string;
+  compose: (a: string, b: string) => string;
+};
+
+const stringMonoid: StringMonoid = {
+  identity: '',
+  compose: (a: string, b: string) => a + b
+};
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>Above, we can see our <code>StringMonoid</code> with an <code>identity</code> value and a <code>compose</code> function.</p>
 <p>We can use the compose function to produce a new string.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/5a095ebd187f2be8098bd5ba2a05c2fa.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+const result1 = stringMonoid.compose(
+  stringMonoid.compose('Hello', ' '),
+  'World'
+);
+// 'Hello World'
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>If we compose with the identity value we get the same value.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/571a8bab94ee65d77013aef7913b9c23.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+const result2 = stringMonoid.compose(
+  stringMonoid.identity,
+  'Hello'
+);
+// 'Hello'
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>Finally, if we call compose on the same arguments it doesn't matter which pair we do first.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/7f76d38e33fbed7a2a82c7a303fec776.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+const result3a = stringMonoid.compose(
+  stringMonoid.compose('a', 'b'),
+  'c'
+);
+
+const result3b = stringMonoid.compose(
+  'a',
+  stringMonoid.compose('b', 'c')
+);
+
+// result3a === result3b === 'abc'
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>Here are some other examples of monoids in JavaScript:</p>
 <ul>
 <li>Adding and multiplying numbers (with 0 and 1 as the identity value respectively).</li>
@@ -94,9 +162,18 @@ const post: Post = {
 <p>Before we dive in let's take a step back. Functors and monoids were both wrappers around a value that allow us to execute operations on them. In the case of functors it was <code>map</code> in the case of monoids it was <code>compose</code>, where compose is a single operation.</p>
 <p>Now monads. Monad's are both a functor and a monoid. That doesn't make this any simpler though. Let's define this in simpler terms.</p>
 <p>A monad is a wrapper around some value that makes it easier to compose functions around it. This is often used to abstract away things like API calls or IO. In fact a <code>Promise</code> is a monad.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/b5d20d2d6c2dace042376db4810336dd.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+&#96;&#96;&#96;typescript
+type Wizard = {
+  name: string;
+  house: string;
+};
+
+const fetchWizard = (id: number): Promise<Wizard> => {
+  return fetch(&#96;/api/wizards/${id}&#96;).then(r => r.json());
+};
+&#96;&#96;&#96;
+<!--kg-card-end: code-->
 <p>If we look at these &nbsp;types above, we have a <code>Wizard</code> and a function that returns a <code>Promise&lt;Wizard&gt;</code>. This promise allows us to compose functions on top of it without worrying about the underlying IO needed to go fetch a <code>Wizard</code> from the server.</p>
 <p>Lets break down further.</p>
 <p>A monad is based on a simple symmetry — A way to wrap a value into a context, and a way to unwrap the value from the context <a href="https://medium.com/javascript-scene/javascript-monads-made-simple-7856be57bfe8">[10]</a>.</p>
