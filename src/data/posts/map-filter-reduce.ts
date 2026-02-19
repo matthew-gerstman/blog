c70279ccc95c354ea572e5883a9e6b6b6aa3de60
Matthew Gerstman <mattgerstman@gmail.com>
2026-02-19 21:25:58 +0000

    fix(map-filter-reduce): inline gists so code renders

--- src/data/posts/map-filter-reduce.ts
+++ src/data/posts/map-filter-reduce.ts
@@ -13,74 +13,228 @@ const post: Post = {
 <h1 id="types">Types</h1>
 <p>Before we dive into map, filter, and reduce, let's set up the list.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/ad7693f7583a07cf038bd38ab931152a.js"></script>
+<pre><code class="language-typescript">const enum House {
+  Gryffindor = "Gryffindor",
+  Hufflepuff = "Hufflepuff",
+  Ravenclaw = "Ravenclaw",
+  Slytherin = "Slytherin"
+}
+
+type Wizard = {
+  name: string;
+  house: House;
+  points: number;
+};</code></pre>
 <!--kg-card-end: html-->
 <p>As per usual, we're going to be operating on wizards. Each wizard object has a name, a house, and the number of points they've earned for their house. Let's declare a bunch of wizards and put them in a list.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/44860c22b93656ab0391243e8f579666.js"></script>
+<pre><code class="language-typescript">const HarryPotter: Wizard = {
+  name: "Harry Potter",
+  house: House.Gryffindor,
+  points: 40
+};
+
+const HermioneGranger: Wizard = {
+  name: "Hermione Granger",
+  house: House.Gryffindor,
+  points: 140
+};
+
+const DracoMalfoy: Wizard = {
+  name: "Draco Malfoy",
+  house: House.Slytherin,
+  points: -20
+};
+
+const TaylorSwift: Wizard = {
+  name: "Taylor Swift",
+  house: House.Slytherin,
+  points: 100
+};
+
+const LinManuelMiranda: Wizard = {
+  name: "Lin Manuel Miranda",
+  house: House.Slytherin,
+  points: 5000
+};
+
+const CedricDiggory: Wizard = {
+  name: "Cedric Diggory",
+  house: House.Hufflepuff,
+  points: 12
+};
+
+const SallyPerks: Wizard = {
+  name: "Sally Perks",
+  house: House.Hufflepuff,
+  points: 15
+};
+
+const LunaLovegood: Wizard = {
+  name: "Luna Lovegood",
+  house: House.Ravenclaw,
+  points: 100
+};
+
+const ChoChang: Wizard = {
+  name: "Cho Chang",
+  house: House.Ravenclaw,
+  points: 100
+};
+
+const wizards: Wizard[] = [
+  HarryPotter,
+  HermioneGranger,
+  DracoMalfoy,
+  LinManuelMiranda,
+  TaylorSwift,
+  CedricDiggory,
+  SallyPerks,
+  LunaLovegood,
+  ChoChang
+];</code></pre>
 <!--kg-card-end: html-->
 <h1 id="map-function map() { [native code] }1">Map</h1>
 <p>Now that we've got the boilerplate out of the way, let's dive in. The first function is the simplest one, map. Map iterates (or loops) over a list, applies a function to each item in that list and then returns a new list of the transformed items. Let's look at an example.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/c02b0634780f0e6c6ef00d5390e6ec42.js"></script>
+<pre><code class="language-typescript">const wizardNames = wizards.map(wizard => wizard.name);</code></pre>
 <!--kg-card-end: html-->
 <p>This function iterates over the list of wizards, gets their name, and puts it into a new array. The result of this looks something like this.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/29ebae139d7839eb981fb110e9c1723f.js"></script>
+<pre><code class="language-typescript">[
+  "Harry Potter",
+  "Hermione Granger",
+  "Draco Malfoy",
+  "Lin Manuel Miranda",
+  "Taylor Swift",
+  "Cedric Diggory",
+  "Sally Perks",
+  "Luna Lovegood",
+  "Cho Chang"
+];</code></pre>
 <!--kg-card-end: html-->
 <p>Now in this example we were using a <a href="/writing/mary-had-a-little-lambda">lambda</a> (aka an anonymous function), but we can also use a named function.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/3d64ba545ca863db2fc3c6abd558abe7.js"></script>
+<pre><code class="language-typescript">function wizardToString({ name, house, points }: Wizard) {
+  return \`\${name}, \${house}, \${points}\`;
+}
+const wizardStrings = wizards.map(wizardToString);
+</code></pre>
 <!--kg-card-end: html-->
 <p>In this example we have a function called <code>wizardToString</code> and we pass that directly to map. It will then return a new list that looks like this.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/bf416ade6e4e95b9ce568a2df5449754.js"></script>
+<pre><code class="language-typescript">[
+  "Harry Potter, Gryffindor, 40",
+  "Hermione Granger, Gryffindor, 140",
+  "Draco Malfoy, Slytherin, -20",
+  "Lin Manuel Miranda, Slytherin, 5000",
+  "Taylor Swift, Slytherin, 100",
+  "Cedric Diggory, Hufflepuff, 12",
+  "Sally Perks, Hufflepuff, 15",
+  "Luna Lovegood, Ravenclaw, 100",
+  "Cho Chang, Ravenclaw, 100"
+];</code></pre>
 <!--kg-card-end: html-->
 <h1 id="filter-function filter() { [native code] }1">Filter</h1>
 <p>Next up is filter. Filter behaves like map in that it iterates over the list, but it instead of transforming each item, it transforms the whole list. Filter takes a function that returns true or false or <a href="/writing/mary-had-a-little-lambda">predicate</a>. It then returns a new list with items where the predicate returns <code>true</code>. Let's look at an example.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/fe6468ae65e0e7a1e11c9d726b09b0ca.js"></script>
+<pre><code class="language-typescript">const slytherins = wizards.filter(wizard => wizard.house === House.Slytherin);</code></pre>
 <!--kg-card-end: html-->
 <p>In this example we filter over the list and only include wizards who are in the house Slytherin. The result would be this.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/048d64ccc3719ba07a2b52a886bf6437.js"></script>
+<pre><code class="language-typescript">[
+  { name: "Draco Malfoy", house: "Slytherin", points: -20 },
+  { name: "Lin Manuel Miranda", house: "Slytherin", points: 5000 },
+  { name: "Taylor Swift", house: "Slytherin", points: 100 }
+];</code></pre>
 <!--kg-card-end: html-->
 <p>As an aside, Taylor and Lin are two of the most acclaimed Slytherins of our day.</p>
 <p>Just like with map, we don't need to use a <a href="/writing/mary-had-a-little-lambda">lambda</a>, we can also use a predefined function.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/884e4116282875442d860d44cb97eac3.js"></script>
+<pre><code class="language-typescript">function isWinner({ points }: Wizard) {
+  return points > 0;
+}
+function isLoser(wizard: Wizard) {
+  return !isWinner(wizard);
+}
+const winners = slytherins.filter(isWinner);
+const losers = slytherins.filter(isLoser);
+</code></pre>
 <!--kg-card-end: html-->
 <p>In this example we make two lists, the list of Slytherins who have earned points (winners), and the list of Slytherins who have lost points (losers). We can see those results below.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/496b2fa8c97d505d79f2aacaa1cb155e.js"></script>
+<pre><code class="language-typescript">const winnersResult = [
+  { name: "Lin Manuel Miranda", house: "Slytherin", points: 5000 },
+  { name: "Taylor Swift", house: "Slytherin", points: 100 }
+];
+const losersResult = [
+  { name: "Draco Malfoy", house: "Slytherin", points: -20 }
+];</code></pre>
 <!--kg-card-end: html-->
 <h1 id="reduce-function reduce() { [native code] }1">Reduce</h1>
 <p>Now we get to the most interesting function, reduce. Reduce iterates over a list and produces a single value. Let's look at an example.</p>
 <p>Suppose we want to get the total number of points across all wizards. We can use reduce to do that.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/39da3b9782f8f3e4a59315c28a3523c9.js"></script>
+<pre><code class="language-typescript">const totalPoints = wizards.reduce(
+  (accumulator, { points }) => accumulator + points,
+  0
+);</code></pre>
 <!--kg-card-end: html-->
 <p>What's going on here? Well reduce is a function that takes two arguments, a function and an initial value for the accumulator. The accumulator is the name of the thing reduce returns. In this case we start our point count at 0.</p>
 <p>Now the function takes the current state of the accumulator and the item in the list it's supposed to process. For the first wizard, it will pass 0 for the accumulator. This function then returns <code>accumulator + points</code>. This will eventually sum up all the points. If you're curious, the result is 5487.</p>
 <p>Now the accumulator can be anything, we can even use reduce to produce an object. Let's look at an example where we add up the points for each house.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/49909068b01b9285c0f18d6177a36b19.js"></script>
+<pre><code class="language-typescript">const pointsPerHouse = wizards.reduce((acc, { house, points }) => {
+  if (!acc[house]) {
+    acc[house] = 0;
+  }
+  acc[house] += points;
+  return acc;
+}, {});</code></pre>
 <!--kg-card-end: html-->
 <p>In this case, we initialize our accumulator or <code>acc</code> with <code>{}</code>. Then for each wizard we call a function that adds the number of points that wizard has earned for their house. If you're curious that result looks like this.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/e4b620c523337b30cb44075b8be2bdd8.js"></script>
+<pre><code class="language-typescript">{
+  Gryffindor: 180,
+  Slytherin: 5080,
+  Hufflepuff: 27,
+  Ravenclaw: 200
+};</code></pre>
 <!--kg-card-end: html-->
 <p>Let's look at one more example, let's say we want the best wizard for each house. We can tweak our previous function to use the best wizard for each house.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/5428d9c0713e7ddab3dfa3a8beb675c6.js"></script>
+<pre><code class="language-typescript">const bestPerHouse = wizards.reduce((acc, wizard) => {
+  const { house, points } = wizard;
+  if (!acc[house]) {
+    acc[house] = wizard;
+  }
+
+  if (acc[house].points < points) {
+    acc[house] = wizard;
+  }
+
+  return acc;
+}, {});</code></pre>
 <!--kg-card-end: html-->
 <p>If you're curious, the result of that looks like this.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/fdac1cee43ba855a48af1abe0ba86629.js"></script>
+<pre><code class="language-typescript">{
+  Gryffindor: { name: "Hermione Granger", house: "Gryffindor", points: 140 },
+  Slytherin: { name: "Lin Manuel Miranda", house: "Slytherin", points: 5000 },
+  Hufflepuff: { name: "Sally Perks", house: "Hufflepuff", points: 15 },
+  Ravenclaw: { name: "Luna Lovegood", house: "Ravenclaw", points: 100 }
+};</code></pre>
 <!--kg-card-end: html-->
 <p>Just a little more fun now, we can use <code>Object.values</code> to transform this map.</p>
 <!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/98890f863a695769975f6eafebd2480b.js"></script>
+<pre><code class="language-typescript">const bestNamesPerHouse = Object.values(bestPerHouse).map(wizardToString);
+[
+  "Hermione Granger, Gryffindor, 140",
+  "Lin Manuel Miranda, Slytherin, 5000",
+  "Sally Perks, Hufflepuff, 15",
+  "Luna Lovegood, Ravenclaw, 100"
+];
+</code></pre>
 <!--kg-card-end: html-->
 <p>Now we have nice strings for the best person in each house.</p>
 <h1 id="wrapping-up">Wrapping Up</h1>
@@ -91,7 +245,7 @@ const post: Post = {
   tags: ['Tech', 'functional-programming'],
   banner_img: '/images/blog/map-filter-reduce/map-filter-reduce-hero.jpg',
   heroImage: '',
-  wordCount: 733,
+  wordCount: 1227,
 };
 
 export default post;
--- src/data/posts/ts-tricks-higher-order-components.ts
+++ src/data/posts/ts-tricks-higher-order-components.ts
@@ -12,40 +12,230 @@ const post: Post = {
 <p>In this case, higher order components are functions that take a component as an argument and return a new supercharged component as a result. This poses an interesting problem for typing.</p>
 <p>We want to expose a component that only needs require the props <em>not</em> provided by the HOC, but we still want to be able to access the original uncomposed component.</p>
 <p>Let's walk through an example.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/343b3bb2726bb1b63c80406ecca419c8.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+type TomRiddleProps = {
+  isEvil: boolean;
+};
+
+const TomRiddle: React.FunctionComponent<TomRiddleProps> = ({ isEvil }) => {
+  return (
+    <div>
+      {isEvil ? <h1>Tom Marvolo Riddle</h1> : <h1>I am Lord Voldemort</h1>}
+    </div>
+  );
+};
+```
+<!--kg-card-end: code-->
 <p>We start with a super basic component, <code>TomRiddle</code>. Now it matters not what someone is born, but what they chose to be. <code>TomRiddle</code> takes an <code>isEvil</code> prop that determines if it renders "Tom Marvolo Riddle" or "I am Lord Voldemort".</p>
 <p>Tom isn't such a great guy and wants to learn how to make horcruxes. How do we teach him? Well we could make him into a class component and add some state. Let's see what that looks like.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/1fa3633692b86a5be34229777d0cae1d.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+type TomRiddleProps = {
+  isEvil: boolean;
+};
+
+type TomRiddleState = {
+  numHorcruxes: number;
+};
+
+class TomRiddle extends React.Component<TomRiddleProps, TomRiddleState> {
+  state = {
+    numHorcruxes: 0
+  };
+  makeHorcrux = () => {
+    this.setState({ numHorcruxes: this.state.numHorcruxes + 1 });
+  };
+  render() {
+    const { isEvil } = this.props;
+    const { numHorcruxes } = this.state;
+    return (
+      <div>
+        {isEvil ? <h1>Tom Marvolo Riddle</h1> : <h1>I am Lord Voldemort</h1>}
+        <span>I have {numHorcruxes} Horcruxes!</span>
+        <button onClick={this.makeHorcrux}>Make Horcrux</button>
+      </div>
+    );
+  }
+}
+```
+<!--kg-card-end: code-->
 <p>This totally works. We've taught Tom how to make horcruxes. Now Tom wants to recruit some followers, he calls those Death Eaters. Let's add another prop for those. While we're at it, let's derive <code>isEvil</code> &nbsp;from the number of horcruxes the wizard has. </p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/008edec62ba9f1a661910071327e554e.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+import * as React from "react";
+
+type TomRiddleProps = {
+  deathEaters: string[];
+};
+
+type TomRiddleState = {
+  numHorcruxes: number;
+};
+
+class TomRiddle extends React.Component<TomRiddleProps, TomRiddleState> {
+  state = {
+    numHorcruxes: 0
+  };
+  makeHorcrux = () => {
+    this.setState({ numHorcruxes: this.state.numHorcruxes + 1 });
+  };
+  render() {
+    const { deathEaters } = this.props;
+    const { numHorcruxes } = this.state;
+
+    const isEvil = numHorcruxes > 0;
+    return (
+      <div>
+        {isEvil ? <h1>Tom Marvolo Riddle</h1> : <h1>I am Lord Voldemort</h1>}
+        <span>I have {numHorcruxes} Horcruxes!</span>
+        <button onClick={this.makeHorcrux}>Make Horcrux</button>
+        {deathEaters != null ? (
+          <>
+            <h2>These are my Death Eaters</h2>
+            <ul>
+              {deathEaters.map(deathEater => (
+                <li key={deathEater}>{deathEater}</li>
+              ))}
+            </ul>
+          </>
+        ) : null}
+      </div>
+    );
+  }
+}
+```
+<!--kg-card-end: code-->
 <p>Now this component is doing a bit too much. It's rendering to the DOM and it's managing state. Also, anyone can learn to make horcruxes, what if we take that functionality and make it into a higher order component. </p>
 <p>Before we get to typing this, let's take a look at what this would look like in vanilla JS.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/9f09e9aed1cc76125902ddbf58e1ad37.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+function EvilWizard(Wizard) {
+  class WithEvilWizard extends React.Component {
+    displayName = `WithEvilWizard(${Wizard.displayName})`;
+    state = {
+      numHorcruxes: 0
+    };
+    makeHorcrux = () => {
+      this.setState({ numHorcruxes: this.state.numHorcruxes + 1 });
+    };
+    render() {
+      const { numHorcruxes } = this.state;
+      return (
+        <Wizard
+          isEvil={numHorcruxes > 0}
+          numHorcruxes={numHorcruxes}
+          makeHorcrux={this.makeHorcrux}
+          {...this.props}
+        />
+      );
+    }
+  }
+  return hoistNonReactStatics(WithEvilWizard, Wizard);
+}
+```
+<!--kg-card-end: code-->
 <p><code>EvilWizard</code> is a function that takes a component, <code>Wizard</code>, and passes that component the following props <code>numHorcruxes</code>, and <code>makeHorcrux</code>. It also manages this state for them.</p>
 <p>Now, two minor gotchas with HOCs, we need to set the <code>displayName</code> and <code>hoistNonReactStatics</code>.</p>
 <p>Okay now let's type this thing. </p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/984fd1a6f9473dca0bb9004ee66b5438.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+import * as React from "react";
+import hoistNonReactStatics = require("hoist-non-react-statics");
+
+type WithEvilWizardProps<T> = T & EvilWizardInnerProps;
+
+type EvilWizardInnerProps = {
+  numHorcruxes: number;
+  makeHorcrux: () => void;
+};
+
+type EvilWizardState = {
+  numHorcruxes: number;
+};
+
+function EvilWizard<T>(
+  Wizard:
+    | React.ComponentClass<WithEvilWizardProps<T>, {}>
+    | React.FunctionComponent<WithEvilWizardProps<T>>,
+): React.ComponentClass<T> {
+  class WithEvilWizard extends React.Component<T, EvilWizardState> {
+    displayName = `WithEvilWizard(${Wizard.displayName})`;
+    state = {
+      numHorcruxes: 0,
+    };
+    makeHorcrux = () => {
+      this.setState({ numHorcruxes: this.state.numHorcruxes + 1 });
+    };
+    render() {
+      return (
+        <Wizard
+          {...this.props}
+          numHorcruxes={this.state.numHorcruxes}
+          makeHorcrux={this.makeHorcrux}
+        />
+      );
+    }
+  }
+  return hoistNonReactStatics(WithEvilWizard, Wizard);
+}
+```
+<!--kg-card-end: code-->
 <p>What's going on here? A lot actually. Let's break it down. We export a type called <code>WithEvilWizardProps</code>. This type takes <code>T</code>, a generic, and returns an intersection type of <code>T</code> and <code>EvilWizardInnerProps</code>.</p>
 <p>The function <code>EvilWizard</code> takes a <code>Wizard</code> that is either a <code>ComponentClass</code> or a <code>FunctionComponent</code>. The props of this component is of type <code>EvilWizardInnerProps &amp; T</code>. Now here's the real magic. The component we're returning is a class of type <code>React.ComponentClass&lt;T&gt;</code>. </p>
 <p>What does this mean? Well because we're using an intersection type, Typescript is smart enough to unpack this and return a component that only requires the props <strong>not</strong> provided by the HOC.</p>
 <p>One last thing, let's update <code>TomRiddle</code> to use this new HOC.</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/12e3e8ed7234df320490df373841a091.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+type TomRiddleProps = WithEvilWizardProps<{
+  deathEaters: string[];
+}>;
+
+const TomRiddle: React.FunctionComponent<TomRiddleProps> = ({
+  deathEaters,
+  numHorcruxes,
+  makeHorcrux,
+}) => {
+  const isEvil = numHorcruxes > 0;
+  return (
+    <div>
+      {isEvil ? <h1>Tom Marvolo Riddle</h1> : <h1>I am Lord Voldemort</h1>}
+      <span>I have {numHorcruxes} Horcruxes!</span>
+      <button onClick={makeHorcrux}>Make Horcrux</button>
+      {deathEaters != null ? (
+        <>
+          <h2>These are my Death Eaters</h2>
+          <ul>
+            {deathEaters.map(deathEater => (
+              <li key={deathEater}>{deathEater}</li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+const LordVoldemort = EvilWizard(TomRiddle);
+```
+<!--kg-card-end: code-->
 <p>We've now extracted all that state back out and we're using <code>WithEvilWizardProps</code> when declaring <code>TomRiddleProps</code>. Now let's use our components!</p>
-<!--kg-card-begin: html-->
-<script src="https://gist.github.com/matthew-gerstman/f63c4028e796863f55f991bb815b5ce0.js"></script>
-<!--kg-card-end: html-->
+<!--kg-card-begin: code-->
+```
+function BothRiddles() {
+  return (
+    <div>
+      <TomRiddle
+        deathEaters={[]}
+        numHorcruxes={0}
+        makeHorcrux={() => undefined}
+      />
+      <LordVoldemort deathEaters={["Bellatrix Lestrange", "Lucius Malfoy"]} />
+    </div>
+  );
+}
+```
+<!--kg-card-end: code-->
 <p>As you can see above, we have the option of using <code>LordVoldemort</code> the output of <code>EvilWizard(TomRiddle)</code> or we can just call <code>TomRiddle</code> directly. This is particularly useful if you want to test the behavior of the inner component without the HOC.</p>
 `.trim(),
   tags: ['Tech'],
